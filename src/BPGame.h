dd8e77bd8b0f67ae1c58319d6a9a1f0698a22703
Thomas Perl <m@thp.io>
2017-08-06 23:28:38 +0200

    Fix const-ness of cmp_str

--- src/BPGame.h
+++ src/BPGame.h
@@ -52,7 +52,7 @@ class BPMiniGame;
 class SpriteFont;
 
 struct cmp_str {
-	bool operator()(char const *a, char const *b) {
+	bool operator()(char const *a, char const *b) const {
 		return std::strcmp(a, b) < 0;
 	}
 };
